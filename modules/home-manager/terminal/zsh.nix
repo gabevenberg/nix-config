97cce59541583ffac870c6e6e7ffc3d468aa1c70
Gabe Venberg <gabevenberg@gmail.com>
2024-04-09 18:20:05 -0500

    carapace completion isnt as good as zsh-native.

--- modules/home-manager/terminal/zsh.nix
+++ modules/home-manager/terminal/zsh.nix
@@ -7,13 +7,28 @@
   programs.fzf.enableZshIntegration = true;
   programs.zoxide.enableZshIntegration = true;
   programs.yazi.enableZshIntegration = true;
-  programs.carapace.enableZshIntegration = true;
   programs.direnv.enableZshIntegration = true;
   programs.kitty.shellIntegration.enableZshIntegration = true;
   services.gpg-agent.enableZshIntegration = true;
+  programs.carapace.enableZshIntegration = false;
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    initExtra = ''
+      #have the menu highlight while we cycle through options
+      zstyle ':completion:*' menu select
+      #case insensitive completion
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+      #allow completion from midword
+      setopt COMPLETE_IN_WORD
+      #move cursor to end of word after completing
+      setopt ALWAYS_TO_END
+      #complete aliases as well
+      setopt COMPLETE_ALIASES
+      #select first item when you press tab the first time.
+      setopt MENU_COMPLETE
+
+    '';
     autocd = true;
     history = {
       ignoreAllDups = true;
