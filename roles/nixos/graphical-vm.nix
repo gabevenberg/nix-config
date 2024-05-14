13a813a32a3ab302e624572d8253c144f2f7bd65
Gabe Venberg <gabevenberg@gmail.com>
2024-05-14 17:27:00 -0500

    apparently spice-autorandr was also needed.

--- roles/nixos/graphical-vm.nix
+++ roles/nixos/graphical-vm.nix
@@ -6,6 +6,7 @@
 }: {
   #note: needs to have something that autostarts desktop files.
   services.spice-vdagentd.enable = true;
+  services.spice-autorandr.enable=true;
   imports = [
     ./vm.nix
   ];
