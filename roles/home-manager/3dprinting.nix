399351c3c1a5b9aed8c695499cd190bc8a86f368
Gabe Venberg <gabevenberg@gmail.com>
2024-05-17 14:38:46 -0500

    added 3d printer software.

---
+++ roles/home-manager/3dprinting.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+# when it gets packaged, will want cadquery and build123d
+  home.packages = with pkgs; [
+    prusa-slicer
+    freecad
+    blender
+    dune3d
+  ];
+}
