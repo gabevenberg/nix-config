1d6c5082b4629633091285028f835073e56cd42f
Gabe Venberg <gabevenberg@gmail.com>
2024-07-25 15:24:11 -0500

    added radicale config.

---
+++ configs/nixos/radicale.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  services.radicale={
+    enable=true;
+    settings={
+      server={
+        hosts=[ "0.0.0.0:5232" "[::]:5232" ];
+      };
+      auth={
+        type="htpasswd";
+        htpasswd_encryption="md5";
+        htpasswd_filename="${inputs.nix-secrets}/radicale-users";
+      };
+    };
+  };
+}
