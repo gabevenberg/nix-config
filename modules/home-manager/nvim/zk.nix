c86b3bac98b174f5b0cced5b7985a173f9640eba
Gabe Venberg <gabevenberg@gmail.com>
2025-02-26 16:46:35 +0100

    added some keybinds for zk-nvim.

--- modules/home-manager/nvim/zk.nix
+++ modules/home-manager/nvim/zk.nix
@@ -7,32 +7,32 @@
   config = lib.mkIf config.user.nvim.enable {
     programs.nixvim = {
       plugins.zk.enable = true;
+      plugins.which-key.settings.spec = [
+        {
+          __unkeyed-1 = "<leader>z";
+          group = "+zk";
+        }
+      ];
+      keymaps = [
+        {
+          action = ":ZkNewFromTitleSelection";
+          key = "<leader>zn";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "New zk note with title from selection";
+          };
+        }
+        {
+          action = ":ZkaMatch";
+          key = "<leader>zs";
+          mode = "n";
+          options = {
+            silent = true;
+            desc = "Search zk notes from selection";
+          };
+        }
+      ];
     };
-    plugins.which-key.settings.spec = [
-      {
-        __unkeyed-1 = "<leader>z";
-        group = "+zk";
-      }
-    ];
-    keymaps = [
-      {
-        action = ":ZkNewFromTitleSelection";
-        key = "<leader>zn";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "New zk note with title from selection";
-        };
-      }
-      {
-        action = ":ZkaMatch";
-        key = "<leader>zs";
-        mode = "n";
-        options = {
-          silent = true;
-          desc = "Search zk notes from selection";
-        };
-      }
-    ];
   };
 }
