10faf084b29f7f9a7bee1d7381a676064ffae645
Gabe Venberg <gabevenberg@gmail.com>
2025-04-29 10:37:58 +0200

    kitty bell config.

--- configs/home-manager/kitty.nix
+++ configs/home-manager/kitty.nix
@@ -13,6 +13,10 @@
     themeFile = "gruvbox-dark";
     settings = {
       background_opacity = "0.8";
+      enable_audio_bell="no";
+      visual_bell_duration = "0.5";
+      window_alert_on_bell="yes";
+      bell_on_tab="🔔";
     };
   };
 }
