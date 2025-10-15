4282c8b19967052bb18c00aeabcf74b659c23d8c
Gabe Venberg <gabevenberg@gmail.com>
2025-10-15 14:57:47 +0200

    removed some redundant comments.

--- hosts/harmatan/disk-config.nix
+++ hosts/harmatan/disk-config.nix
@@ -22,8 +22,7 @@
               content = {
                 type = "luks";
                 name = "crypted";
-                # disable settings.keyFile if you want to use interactive password entry
-                passwordFile = "/tmp/secret.key"; # Interactive
+                passwordFile = "/tmp/secret.key";
                 content = {
                   type = "btrfs";
                   extraArgs = ["-L" "nixos" "-f"];
