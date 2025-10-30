a4b6b50987d3b872af0be133b2fefab2fa8f4c6a
Gabe Venberg <gabevenberg@gmail.com>
2025-10-30 23:01:07 +0100

    changed background animation of ly.

--- configs/nixos/graphical.nix
+++ configs/nixos/graphical.nix
@@ -10,5 +10,9 @@
   services.xserver.xkb.options = "ctrl:nocaps,compose:rctrl";
   services.displayManager.ly = {
     enable = true;
+    settings = {
+      animation = "matrix";
+      bigclock = "en";
+    };
   };
 }
