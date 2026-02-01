0aa0f21756ca1198af761e51cf0f0e2b6310c141
Gabe Venberg <gabevenberg@gmail.com>
2026-02-01 11:24:35 +0100

    enabled screen locking on wake up.

--- configs/nixos/i3/lockscreen.nix
+++ configs/nixos/i3/lockscreen.nix
@@ -4,6 +4,20 @@
   lib,
   ...
 }: {
+  systemd.services.betterlockscreen = {
+    enable = true;
+    description = "Locks screen when going to sleep/suspend";
+    environment = {DISPLAY = "0";};
+    serviceConfig = {
+      User = config.host.details.user;
+      alias = ["betterlockscreen@${config.host.details.user}.service"];
+      Type = "simple";
+      ExecStart = ''${pkgs.betterlockscreen}/bin/betterlockscreen --lock dim'';
+      TimeoutSec = "infinity";
+    };
+    wantedBy = ["sleep.target" "suspend.target"];
+  };
+
   home-manager.users.${config.host.details.user} = {
     config,
     osConfig,
@@ -23,6 +37,7 @@
         '';
       };
     };
+    # Define systemd service for betterlockscreen to run on suspend
     imports = [
       ../../home-manager/feh.nix
     ];
